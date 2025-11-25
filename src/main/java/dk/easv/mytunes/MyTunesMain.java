904a3a98a629706b99f7d03c2ce1ea38e5d22795
Felix Runge <>
2025-11-25 10:12:32 +0100

    Slght updates to UI. Better flexibility

--- src/main/java/dk/easv/mytunes/MyTunesMain.java
+++ src/main/java/dk/easv/mytunes/MyTunesMain.java
@@ -12,7 +12,7 @@ public class MyTunesMain extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MyTunesMain.class.getResource("MyTunesView.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("MyTunes");
         stage.setScene(scene);
         stage.show();
     }
