ec7844dbbf53b86769320c88293026729aa51352
Felix Runge <>
2025-11-25 10:01:02 +0100

    Made getters and setters

---
+++ src/main/java/dk/easv/mytunes/be/Song.java
@@ -0,0 +1,90 @@
+package dk.easv.mytunes.be;
+
+public class Song {
+
+    private int id;
+    private String title;
+    private String artist;
+    private String category;
+    private int duration;
+    private String filePath;
+
+    public Song(int id, String title, String artist, String category, int duration, String filePath) {
+        this.id = id;
+        this.title = title;
+        this.artist = artist;
+        this.category = category;
+        this.duration = duration;
+        this.filePath = filePath;
+    }
+
+    public Song(String title, String artist, String category, int duration, String filePath) {
+        this(-1,title,artist,category, duration,filePath);
+    }
+
+    // Getters and setters
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    /**
+     * Formats the time as MM:SS
+     * @return time in minutes and seconds
+     */
+    public String getFormattedDuration() {
+        int minutes = duration / 60;
+        int seconds = duration % 60;
+        return String.format("%d:%02d", minutes, seconds);
+    }
+
+    @Override
+    public String toString() {
+        return title + " - " + artist;
+    }
+}
