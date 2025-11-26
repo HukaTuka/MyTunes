21ae25dea84d68baf2e6f0e595955fc73f3d80e1
Tobias <tobrie01@easv365.dk>
2025-11-26 09:55:48 +0100

    implemented interface methods

--- src/main/java/dk/easv/mytunes/dal/PlaylistDAO.java
+++ src/main/java/dk/easv/mytunes/dal/PlaylistDAO.java
@@ -1,4 +1,28 @@
 package dk.easv.mytunes.dal;
 
-public class PlaylistDAO {
+import dk.easv.mytunes.be.Playlist;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class PlaylistDAO implements IPlaylistDataAccess {
+    @Override
+    public List<Playlist> getAllPlaylists() throws SQLException {
+        return List.of();
+    }
+
+    @Override
+    public Playlist createPlaylist(Playlist newPlaylist) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public void updatePlaylist(Playlist playlist) throws SQLException {
+
+    }
+
+    @Override
+    public void deletePlaylist(Playlist playlist) throws SQLException {
+
+    }
 }
