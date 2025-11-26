8941ace912e012c9e0ed7dbebe69edb31b6f3129
Felix Runge <>
2025-11-26 10:05:48 +0100

    Implemented playlist-song relations methods

--- src/main/java/dk/easv/mytunes/dal/IPlaylistDataAccess.java
+++ src/main/java/dk/easv/mytunes/dal/IPlaylistDataAccess.java
@@ -9,11 +9,17 @@ import java.util.List;
 
 public interface IPlaylistDataAccess {
 
+    //Playlist operations
     List<Playlist> getAllPlaylists() throws SQLException;
     Playlist createPlaylist(Playlist newPlaylist) throws SQLException;
     void updatePlaylist(Playlist playlist) throws SQLException;
     void deletePlaylist(Playlist playlist) throws SQLException;
 
-
+    //Playlist-Song relationship operations
+    List<Song> getSongsInPlaylist(int playlistId) throws SQLException;
+    void addSongToPlaylist(int playlistId, int songId) throws SQLException;
+    void removeSongFromPlaylist(int playlistId, int songId) throws SQLException;
+    void moveSongUp(int playlistId, int songId) throws SQLException;
+    void moveSongDown(int playlistId, int songId) throws SQLException;
 
 }
--- src/main/java/dk/easv/mytunes/dal/PlaylistDAO.java
+++ src/main/java/dk/easv/mytunes/dal/PlaylistDAO.java
@@ -1,6 +1,7 @@
 package dk.easv.mytunes.dal;
 
 import dk.easv.mytunes.be.Playlist;
+import dk.easv.mytunes.be.Song;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -23,6 +24,31 @@ public class PlaylistDAO implements IPlaylistDataAccess {
 
     @Override
     public void deletePlaylist(Playlist playlist) throws SQLException {
+
+    }
+
+    @Override
+    public List<Song> getSongsInPlaylist(int playlistId) throws SQLException {
+        return List.of();
+    }
+
+    @Override
+    public void addSongToPlaylist(int playlistId, int songId) throws SQLException {
+
+    }
+
+    @Override
+    public void removeSongFromPlaylist(int playlistId, int songId) throws SQLException {
+
+    }
+
+    @Override
+    public void moveSongUp(int playlistId, int songId) throws SQLException {
+
+    }
+
+    @Override
+    public void moveSongDown(int playlistId, int songId) throws SQLException {
 
     }
 }
