940d32ba020354916bc037497aa3caf373eb0afa
rekast <renste01@easv365.dk>
2025-11-26 09:48:05 +0100

    Renmaed to IPlaylistDataAccess

--- src/main/java/dk/easv/mytunes/dal/IPlaylistDataAccess.java
+++ src/main/java/dk/easv/mytunes/dal/IPlaylistDataAccess.java
@@ -7,15 +7,7 @@ import dk.easv.mytunes.be.Song;
 import java.sql.SQLException;
 import java.util.List;
 
-public interface IMytunesDataAccess {
-
-    // Song operations
-    Song createSong(Song song) throws SQLException;
-    List<Song> getAllSongs() throws Exception;
-    Song getSongById(int id) throws SQLException;
-    void updateSong(Song song) throws SQLException;
-    void deleteSong(int songId) throws SQLException;
-    List<Song> searchSongs(String query) throws SQLException;
+public interface IPlaylistDataAccess {
 
     List<Playlist> getAllPlaylists() throws SQLException;
     Playlist createPlaylist(Playlist newPlaylist) throws SQLException;
