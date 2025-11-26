2b519cb7a2bddf729bf70d476221855d32d3648b
Felix Runge <>
2025-11-26 10:11:42 +0100

    Renamed file

--- src/main/java/dk/easv/mytunes/gui/controller/MyTunesViewController.java
+++ src/main/java/dk/easv/mytunes/gui/controller/MyTunesViewController.java
@@ -4,7 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
-public class MyTunesController {
+public class MyTunesViewController {
 
     @FXML
     private TableView<?> tblPl;
